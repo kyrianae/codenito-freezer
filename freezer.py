ca7a9a6e0aef85be33abca6f8ff937d2455a63bc
kyrianae <kyrianae@gmail.com>
2023-04-16 18:48:19 +0200

    add loguru use

--- freezer.py
+++ freezer.py
@@ -2,6 +2,7 @@
 from flask import Flask, json, request, Response
 import random
 import datetime
+from loguru import logger
 
 app = Flask("pizzaiolo")
 name="Freeze"
@@ -10,32 +11,32 @@ def str_now():
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    print (str_now()+"\t"+name+"says: Hello World")
+    logger.info (str_now()+"\t"+name+"\tsays: Hello World")
     return str_now()+"\t"+name+"says: Hello World\n"
 
 @app.route('/pie', methods=['GET'])
 def pie():
-    print (str_now()+"\t"+name+"\tpie")
+    logger.info (str_now()+"\t"+name+"\tpie")
     return str_now()+"\t"+name+"\tpie\n"
 
 @app.route('/cheese', methods=['GET'])
 def cheese():
-    print (str_now()+"\t"+name+"\tcheese")
+    logger.info (str_now()+"\t"+name+"\tcheese")
     return str_now()+"\t"+name+"\tcheese\n"
 
 @app.route('/tomato', methods=['GET'])
 def tomato():
-    print (str_now()+"\t"+name+"\ttomato")
+    logger.info (str_now()+"\t"+name+"\ttomato")
     return str_now()+"\t"+name+"\ttomato\n"
 
 @app.route('/ham', methods=['GET'])
 def ham():
-    print (str_now()+"\t"+name+"\tham")
+    logger.info (str_now()+"\t"+name+"\tham")
     return str_now()+"\t"+name+"\tham\n"
 
 @app.route('/mushroom', methods=['GET'])
 def mushroom():
-    print (str_now()+"\t"+name+"\tmuhsroom")
+    logger.info (str_now()+"\t"+name+"\tmuhsroom")
     return str_now()+"\t"+name+"\tmushroom\n"
 
 if __name__ == '__main__':
